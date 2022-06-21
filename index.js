36aa5b95d5579cc57428eecf9bf0790e38315884
Kuldeep Kumar <kuldeep.kumar@zendesk.com>
2022-06-21 17:52:27 +0100

    changing app name

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const ws = require("is-wasabi-npm-package")
 
 console.log("calling dependencies")
 console.log(ws.getLength("kkk"))
+console.log(ws.getLength("kuldeep"))
 console.log(ws.isWasabi("wasabi"))
 console.log(ws.creator)
