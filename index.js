abef67ec7c857b0f578487a69f6c63fdf650cc97
Kuldeep Kumar <kuldeep.kumar@zendesk.com>
2022-06-23 11:54:34 +0100

    trying to call 2.9.9

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const ws = require("is-wasabi-npm-package")
+// const ws = require("is-wasabi-npm-package")
 
-console.log("calling dependencies")
-console.log(ws.getLength("kkk"))
-console.log(ws.getLength("kuldeep"))
-console.log(ws.isWasabi("wasabi"))
-console.log(ws.creator)
+// console.log("calling dependencies")
+// console.log(ws.getLength("kkk"))
+// console.log(ws.getLength("kuldeep"))
+// console.log(ws.isWasabi("wasabi"))
+// console.log(ws.creator)
+
+
+const dino = require("@it-eng/is-dinosaur")
+
+console.log("calling is-dinosaur package")
+console.log(dino.isDinosaur("dinosaur"))
+console.log(dino.isDinosaur("dinosaur"))
+console.log(dino.isDinosaur("dinosaur-not"))
